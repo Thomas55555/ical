37828b3312289adb2e7d471af4d9d0e1e619abc4
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2025-03-29 20:09:34 +0100

    Update utc_offset.py

--- ical/types/utc_offset.py
+++ ical/types/utc_offset.py
@@ -11,8 +11,8 @@ from ical.parsing.property import ParsedProperty
 
 from .data_types import DATA_TYPE
 
-UTC_OFFSET_REGEX = re.compile(r"^([-+]?)([0-9]{2})([0-9]{2})$")
-
+# Updated regex to support optional seconds
+UTC_OFFSET_REGEX = re.compile(r"^([-+]?)([0-9]{2})([0-9]{2})([0-9]{2})?$")
 
 @DATA_TYPE.register("UTC-OFFSET")
 @dataclass
@@ -31,10 +31,11 @@ class UtcOffset:
             value = prop.value
         if not (match := UTC_OFFSET_REGEX.fullmatch(value)):
             raise ValueError(f"Expected value to match UTC-OFFSET pattern: {value}")
-        sign, hours, minutes = match.groups()
+        sign, hours, minutes, seconds = match.groups()
         result = datetime.timedelta(
-            hours=int(hours or 0),
-            minutes=int(minutes or 0),
+            hours=int(hours),
+            minutes=int(minutes),
+            seconds=int(seconds or 0),
         )
         if sign == "-":
             result = -result
@@ -45,16 +46,18 @@ class UtcOffset:
         """Serialize a time delta as a UTC-OFFSET ICS value."""
         duration = value.offset
         parts = []
-        if duration < datetime.timedelta(days=0):
+        if duration < datetime.timedelta():
             parts.append("-")
             duration = -duration
         else:
             parts.append("+")
-        seconds = duration.seconds
-        hours = int(seconds / 3600)
-        seconds %= 3600
+        total_seconds = duration.seconds
+        # Assuming offsets are less than 24 hours.
+        hours = total_seconds // 3600
+        total_seconds %= 3600
+        minutes = total_seconds // 60
+        seconds = total_seconds % 60
         parts.append(f"{hours:02}")
-        minutes = int(seconds / 60)
-        seconds %= 60
         parts.append(f"{minutes:02}")
+        parts.append(f"{seconds:02}")
         return "".join(parts)
